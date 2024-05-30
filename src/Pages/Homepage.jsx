7976dd75673f430e4ea5b7b4c5e648f129475bec
MAGGY-GIT <kairumaggie29@gmail.com>
2024-05-30 17:16:24 +0300

    changes col-responsiveness in homepage and modified gitignore file to hide api information

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Homepage() {
   return (
-    <div>
-      <div>Genre</div>
-      <div>Movie & Series List</div>
+    <div className='grid grid-cols-4'>
+      <div className='bg-red-500 h-full hidden md:block'>Genre</div>
+      <div className=' col-span-4 md:col-span-3 bg-cyan-500'>Movie & Series List</div>
     </div>
   )
 }
