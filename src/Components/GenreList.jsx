c3065caf4eaf8af30a41edadd343c37c522b26a4
MAGGY-GIT <kairumaggie29@gmail.com>
2024-05-30 22:09:21 +0300

    added .genres to json to show the genres in console.log

--- src/Components/GenreList.jsx
+++ src/Components/GenreList.jsx
@@ -6,7 +6,8 @@ function GenreList() {
   const getGenreList = ()=>{
     fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=c7e5e005fe52947a29f48774c04e5309")
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => console.log(json.genres))
+    /**shows the 19 genres specifically out of the class genres */
   }
 
   useEffect(()=>{
