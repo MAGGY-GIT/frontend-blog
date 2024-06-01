4f745969e8d5ccea7355b348e99ac0c214d0341a
MAGGY-GIT <kairumaggie29@gmail.com>
2024-06-01 22:00:13 +0300

    finalized linking the contacts and homepage using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import './App.css'
-import Navbar from './Components/Navbar'
-import Contacts from './Pages/Contacts'
-import Homepage from './Pages/Homepage'
-import {Routes, Route} from 'react-router-dom'
-
+import './App.css';
+import Navbar from './Components/Navbar';
+import Contacts from './Pages/Contacts';
+import Homepage from './Pages/Homepage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div>
-      <Navbar/>
-      {/**anything out of the routes tage will appear on all pages */}
+    <>
+      <Navbar />
+      {/** Anything out of the routes tag will appear on all pages */}
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
-        <Route path='/contacts' element={<Contacts/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
-      
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
